2fb3fed077bf455e5cc01b5e92fc5f985e7d6d4a
dipshanrana <dipshanranabhat09@gmail.com>
2026-08-12 15:49:14 +0545

    calculator:

--- src/com/dipshan/Calculator.java
+++ src/com/dipshan/Calculator.java
@@ -5,8 +5,46 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
+        int ans = 0;
 
+        while(true){
+            System.out.println("Enter the operator: ");
+            char op = sc.next().trim().charAt(0);
+
+            if(op == '+' || op == '-' || op == '*' || op == '%' || op == '/'){
+                System.out.println("Enter the numbers: ");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+
+                if(op == '+'){
+                    ans = num1 + num2;
+                }
+
+                if(op == '*'){
+                    ans = num1 * num2;
+                }
+
+                if(op == '/') {
+                    if (num2 == 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if(op == '%'){
+                    ans = num1%num2;
+                }
+
+                if (op == '-'){
+                  ans = num1-num2;
+                }
+                }
+
+            else if (op=='x' || op == 'X') {
+              break;
+            }
+            else{
+                System.out.println("Invalid operation!! ");
+            }
+            System.out.println(ans);
         }
 
     }
