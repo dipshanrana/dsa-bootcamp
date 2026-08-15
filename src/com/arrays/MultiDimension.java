ba12d30958b93d5564ea2d7123da98ead99f3953
dipshanrana <dipshanranabhat09@gmail.com>
2026-08-15 23:24:33 +0545

    multidimension array

--- src/com/arrays/MultiDimension.java
+++ src/com/arrays/MultiDimension.java
@@ -1,5 +1,27 @@
 package com.arrays;
 
-public class MultiDimension {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class MultiDimension {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][] arr = new int [3][4];
+
+        int [] [] arr1 = {
+                {1,2},
+                {3,4,5},
+                {3,4,5,6}
+        };
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(arr1[0][1]);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j]= sc.nextInt();
+            }
+        }
+
+    }
 }
