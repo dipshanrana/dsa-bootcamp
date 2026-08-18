27e76110cf589b8d09710996fad0183e727904b9
dipshanrana <dipshanranabhat09@gmail.com>
2026-08-18 12:14:21 +0545

    array reversing

--- src/com/arrays/Max.java
+++ src/com/arrays/Max.java
@@ -2,12 +2,18 @@ package com.arrays;
 
 public class Max {
     public static void main(String[] args) {
-        int[] arr = {1,3,23,9,18};
+        int[] arr = {};
         System.out.println(max(arr));
         System.out.println(maxRange(arr,0,2));
+
     }
     static int max(int[] arr){
+
+        if(arr.length==0){
+            return -1;
+        }
     int max = arr[0];
+
 //    for(int e : arr){
 //        if(e>max){
 //            max = e;
@@ -22,6 +28,12 @@ public class Max {
     return max;
     }
     static int maxRange(int[] arr,int start, int end ){
+        if(end>start){
+            return -1;
+        }
+        if(arr.length==0){
+            return -1;
+        }
        int max = arr[start];
         for (int i = start+1; i <= end; i++) {
             if(arr[i]>max){
@@ -30,4 +42,6 @@ public class Max {
         }
         return max;
     }
+
+
 }
--- src/com/arrays/Swap.java
+++ src/com/arrays/Swap.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class Swap {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,44,55};
-        swap(arr,1,3);
+        int [] arr = {1,2,3,44,55,31};
+//        swap(arr,1,3);
+        System.out.println(Arrays.toString(arr));
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void swap(int[] arr,int index1, int index2){
@@ -13,4 +15,21 @@ public class Swap {
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
+    static void reverse(int[] arr){
+        int count = 1;
+//        while(count<=arr.length/2){
+//            swap(arr,count-1,arr.length-count);
+//            count++;
+//        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+
+    }
+
 }
