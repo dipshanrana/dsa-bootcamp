6b8feef5e6aa1ff071b7ae351da3d4d1afca7710
dipshanrana <dipshanranabhat09@gmail.com>
2026-08-19 20:40:11 +0545

    commenting

--- src/com/arrays/Swap.java
+++ src/com/arrays/Swap.java
@@ -1,35 +1,35 @@
-package com.arrays;
-
-import java.util.Arrays;
-
-public class Swap {
-    public static void main(String[] args) {
-        int [] arr = {1,2,3,44,55,31};
-//        swap(arr,1,3);
-        System.out.println(Arrays.toString(arr));
-        reverse(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-    static void swap(int[] arr,int index1, int index2){
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-    static void reverse(int[] arr){
-        int count = 1;
-//        while(count<=arr.length/2){
-//            swap(arr,count-1,arr.length-count);
-//            count++;
+//package com.arrays;
+//
+//import java.util.Arrays;
+//
+//public class Swap {
+//    public static void main(String[] args) {
+//        int [] arr = {1,2,3,44,55,31};
+////        swap(arr,1,3);
+//        System.out.println(Arrays.toString(arr));
+//        reverse(arr);
+//        System.out.println(Arrays.toString(arr));
+//    }
+//    static void swap(int[] arr,int index1, int index2){
+//        int temp = arr[index1];
+//        arr[index1] = arr[index2];
+//        arr[index2] = temp;
+//    }
+//    static void reverse(int[] arr){
+//        int count = 1;
+////        while(count<=arr.length/2){
+////            swap(arr,count-1,arr.length-count);
+////            count++;
+////        }
+//        int start = 0;
+//        int end = arr.length - 1;
+//
+//        while(start<end){
+//            swap(arr,start,end);
+//            start++;
+//            end--;
 //        }
-        int start = 0;
-        int end = arr.length - 1;
-
-        while(start<end){
-            swap(arr,start,end);
-            start++;
-            end--;
-        }
-
-    }
-
-}
+//
+//    }
+//
+//}
